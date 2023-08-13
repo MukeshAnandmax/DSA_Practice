bf2fb85213edf27daca0cc9cf3fd1ffb343c2832
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-08-13 16:23:43 +0530

    13Aug Q1

--- LinkedList/src/Day1.java
+++ LinkedList/src/Day1.java
@@ -1,2 +1,35 @@
 public class Day1 {
+
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * public int val;
+     * public ListNode next;
+     * ListNode(int x) { val = x; next = null; }
+     * }
+     */
+    public class Solution {
+
+/*You are given A which is the head of a linked list. Print the linked list in space separated manner.
+
+Note : The last node value must also be succeeded by a space and after printing the entire list you should print a new line
+Problem Constraints
+1 <= size of linked list <= 105
+
+1 <= value of nodes <= 109
+*/
+        public void solve(ListNode A) {
+
+
+            while (A != null) {
+                System.out.print(A.val + " ");
+                A = A.next;
+            }
+            System.out.println();
+
+        }
+    }
+
+
 }
